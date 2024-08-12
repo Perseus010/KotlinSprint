30d43f4879b0c466f941b2cd501e5528432becef
Admin <abaranov@>
2024-08-12 20:31:42 +0300

    KS-1-1 correction format code

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,9 +1,8 @@
 package org.example.lesson_1
 
 fun main() {
-    //объявление переменных
 
-    val numberOfOrders : Int = 75
-    val textThankForBuy : String = "Спасибо за покупку!"
+    val numberOfOrders: Int = 75
+    val textThankForBuy: String = "Спасибо за покупку!"
 
 }
