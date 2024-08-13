fc471c4bd325fc12bae9c874bd1b32f40fc100fb
Admin <abaranov@>
2024-08-13 14:18:44 +0300

    KS-1-2 arithmetic operations

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val textThankForBuy: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(textThankForBuy)
+
+    var numberOfWorkers: Int = 2000
+
+    //println(numberOfWorkers)
+
+    numberOfWorkers = numberOfWorkers - 1
+
+    println(numberOfWorkers)
+
+}
